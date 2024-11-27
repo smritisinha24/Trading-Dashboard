91df7b6f9491e34741d6d0873d274c01080c1337
Smriti Sinha <116907906+smritisinha24@users.noreply.github.com>
2024-11-27 18:57:25 +0530

    Delete Backend directory

--- Backend/src/main/java/com/CME/backend/repository/PriceInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.CME.backend.repository;
-
-import com.CME.backend.model.PriceInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriceInfoRepository extends JpaRepository<PriceInfo, String> {
-    PriceInfo findBySymbolIgnoreCase(String symbol);
-}
